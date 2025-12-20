9d29f71f69c823142157d78bc6e2df6becd1edd8
Muneeb Barry <muneebbarry258@gmail.com>
2025-12-20 13:27:24 +0500

    api fixed

--- src/lib/api-client.jsx
+++ src/lib/api-client.jsx
@@ -1,6 +1,5 @@
 // const API_BASE_URL = "http://10.0.0.234:4000/api";
 // const API_BASE_URL = "http://localhost:4000/api";
-// abd
 const API_BASE_URL = "https://backend-task-track.onrender.com/api";
 
 export const apiClient = {
