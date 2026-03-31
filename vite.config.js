7156fd2f34e6dcdf75a727e9c0615df63f55cd20
Usman Abdullah <usmanabdullahbn@gmail.com>
2026-03-31 09:31:15 +0500

    fixing

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,11 @@
-import { defineConfig } from 'vite'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "vite";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  plugins: [
-    tailwindcss(),
-  ],
+  plugins: [tailwindcss()],
   server: {
     host: true, // or '0.0.0.0'
-    port: 5173
-  }
-})
+    port: 5173,
+    allowedHosts: ["www.sgitt.com"], // ✅ add this line
+  },
+});
